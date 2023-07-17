d941f7d4f42b1906c92084dde3eedf2215c13fc5
Anu9969 <sharma.22552@sscbs.du.ac.in>
2023-07-17 16:09:41 +0530

    added java

---
+++ first.java
@@ -0,0 +1,19 @@
+public class first{
+    public static void main(String[] args) {
+        // System.out.println("Hello People");
+        // System.out.println("*****");
+        // System.out.println("****");
+        // System.out.println("***");
+        // System.out.println("**");
+        // System.out.println("*");
+
+
+        int a = 5;
+        int b = 9;
+        int sum = (a + b) ;
+        System.out.println(sum);
+
+
+
+    }
+}
